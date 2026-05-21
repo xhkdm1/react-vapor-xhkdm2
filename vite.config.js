4ad60153f5dbd928fc5f59b33093f759b2ec0d83
xhkdm1 <your-xhkdm1116@gmail.com>
2026-05-21 09:27:37 +0900

    코덱스연습

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", //github에 레포로 뜨게 하기 위함 
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
